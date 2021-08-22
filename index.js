14da3f4f3c13822d63ed93d54a358ce17b2e85a9
Sunil Tamang <52387595+sunielghising@users.noreply.github.com>
2021-08-22 20:18:58 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var example = ['an undergraduate student.',
+  'a front-end developer.',
+  'a back-end developer.',
+  'a full-stack developer.',
+  'a mobile app developer.'
+];
+textSequence(0);
+
+function textSequence(i) {
+
+  if (example.length > i) {
+    setTimeout(function() {
+      $(".role").text("I am " + example[i]);
+      textSequence(++i);
+    }, 1500); // 1 second (in milliseconds)
+
+  } else if (example.length == i) { // Loop
+    textSequence(0);
+  }
+
+}
