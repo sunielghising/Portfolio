8ff191dcb4c15b16bee42ba33152a63271414714
Sunil Tamang <52387595+sunielghising@users.noreply.github.com>
2021-08-31 22:06:47 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 //navbar active link
 $(document).ready(function () {
 
-           $('ul.nav-list> li')
+           $('li.nav-item>a')
                    .click(function (e) {
-               $('ul.nav-list> li')
+               $('li.nav-item>a')
                    .removeClass('active');
                $(this).addClass('active');
            });
        });
 
-
 //loop of job-roles
-var example = ['an undergraduate student.',
-  'a front-end developer.',
-  'a back-end developer.',
-  'a full-stack developer.',
-  'a mobile app developer.'
+var example = [
+  'front-end',
+  'back-end',
+  'full-stack',
+  'mobile app'
 ];
 textSequence(0);
 
@@ -23,7 +22,7 @@ function textSequence(i) {
 
   if (example.length > i) {
     setTimeout(function() {
-      $(".role").text("I am " + example[i]);
+      $(".role").text(example[i]);
       textSequence(++i);
     }, 1500); // 1 second (in milliseconds)
 
